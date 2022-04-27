a1b08608654915d59e3c6e4072bd571f18e7af98
Lyle Jensen <lyle.jensen95@gmail.com>
2022-04-27 16:25:01 -0600

    added json parser and room class #AB112 #AB113 #AB114 #AB115

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,13 +3,16 @@ import java.io.IOException;
 public class Game {
 
     void runGame(){
-        MainMenu menu = new MainMenu();
-        try {
-            menu.showMainMenu();
-        } catch (
-                IOException e) {
-            e.printStackTrace();
-        }
+//        MainMenu menu = new MainMenu();
+//        try {
+//            menu.showMainMenu();
+//        } catch (
+//                IOException e) {
+//            e.printStackTrace();
+//        }
+
+        GameData gameData = new GameData();
+        gameData.loadGame();
     }
 
 }
