0016090a5da7e1f4f1cd3b2ad2e5857238c617b2
Lyle Jensen <lyle.jensen95@gmail.com>
2022-04-27 23:19:36 -0600

    added NPC class, created fields for Room class, and created json reader method

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,5 +1,17 @@
 public class Room {
     public String name = null;
     public String description = null;
+    public String npc = null;
 
+    String getName() {
+        return this.name;
+    }
+
+    String getDescription() {
+        return this.description;
+    }
+
+    String getNpc() {
+        return this.npc;
+    }
 }
