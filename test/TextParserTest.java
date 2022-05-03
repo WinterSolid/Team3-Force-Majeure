f1faf57ac9491552ea85f99d312b62db67cad32a
Tomas <tomas.huzevka@outlook.com>
2022-05-03 13:52:40 -0400

    added unit tests Inventory TextParser and rooms.json

--- test/TextParserTest.java
+++ test/TextParserTest.java
@@ -1,19 +1,36 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import static org.junit.Assert.*;
 
+// user for testing user input at command prompt
 public class TextParserTest {
-
-    @Test
-    public void textInputMainMenuValidStart() {
-
+    public InputStream userIn(String testCase){
+        InputStream in = new ByteArrayInputStream(testCase.getBytes());
+        return in;
     }
 
+//    TODO textInputMainMenuValidQuit and textInputMainMenuInvalid
     @Test
-    public void gameScannerInput() {
-
+    public void testTextInputMainMenuValidStart() {
+//        start returns "game" which starts game
+        InputStream in = userIn("start");
+        System.setIn(in);
+        String answer = TextParser.textInputMainMenu();
+        assertEquals("game", answer);
     }
-
+//    TODO testGameScannerInputInvalid testGameScannerInputHelp testGameScannerInputQuit
     @Test
-    public void gameScannerOutput() {
+    public void testGameScannerInputTwoWord() throws IOException {
+//        returns 2 word command
+        InputStream in = userIn("go north");
+        System.setIn(in);
+        String answer = TextParser.gameScannerInput();
+        assertEquals("go north", answer);
     }
+//    TODO testGameScannerOutput
 }
